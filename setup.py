c5776b90aea57d6ee3387fd2fcba8aaa44d8ffab
Chris-22ozor <chukoz71@gmail.com>
2025-10-05 02:29:28 +0100

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages 
+
+setup(
+
+    name="us_visa",
+    version="0.0.0",
+    author="Chris",
+    author_email="chrisyahoomail.com",
+    packages=find_packages()
+)
